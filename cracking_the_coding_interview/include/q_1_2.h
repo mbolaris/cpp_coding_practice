83421e834799672a085f9c49f774f99f13bfa576
Mike Bolaris <mike@bolaris.com>
2017-10-12 21:46:23 -0700

    Added Cracking the Coding Interview Q1.2

---
+++ cracking_the_coding_interview/include/q_1_2.h
@@ -0,0 +1,17 @@
+/*
+ * q_1_2.h
+ *
+ *  Copyright 2017 Bolaris Engineering
+ *
+ */
+
+#ifndef CRACKING_THE_CODING_INTERVIEW_INCLUDE_Q_1_1_H_
+#define CRACKING_THE_CODING_INTERVIEW_INCLUDE_Q_1_1_H_
+
+#include <string>
+
+namespace q_1_2 {
+    bool isPermutation(const std::string &strA, const std::string &strB);
+};
+
+#endif /* CRACKING_THE_CODING_INTERVIEW_INCLUDE_Q_1_1_H_ */
---
+++ cracking_the_coding_interview/test/test_q_1_2.cpp
@@ -0,0 +1,24 @@
+/*
+ * fibonacci_test.cpp
+ *
+ *  Copyright 2017 Bolaris Engineering
+ *
+ */
+
+#include "../include/q_1_2.h"
+#include "gtest/gtest.h"
+
+TEST(q_1_2, isPermutation) {
+    EXPECT_EQ(q_1_2::isPermutation("abcd", "dbca"), true);
+    EXPECT_EQ(q_1_2::isPermutation("abca", "aaaa"), false);
+    EXPECT_EQ(q_1_2::isPermutation("KaiBolarisIsAwesome!", "xxxxxxxxxxxxxxxxxxxx"), false);
+    EXPECT_EQ(q_1_2::isPermutation("", "xxxxxxxxxxxxxxxxxxxx"), false);
+    EXPECT_EQ(q_1_2::isPermutation("", ""), true);
+}
+
+
+//int main(int argc, char **argv) {
+//    testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
+
