90eaaca7044e8f55bec771d67c14e7fb8bd8dc96
Mike Bolaris <mike@bolaris.com>
2017-10-12 21:50:00 -0700

    Added Cracking the Coding Interview Q1.2

---
+++ cracking_the_coding_interview/src/q_1_2.cpp
@@ -0,0 +1,38 @@
+/*
+ * q_1_2.h
+ *
+ *  Copyright 2017 Bolaris Engineering
+ *
+ *  Given two strings, write a method to decide if one is a permutation of the other.
+ *
+ */
+
+#include "../include/q_1_2.h"
+#include <string>
+
+namespace q_1_2 {
+
+    bool isPermutation(const std::string &strA, const std::string &strB) {
+        if (strA.length() != strB.length()) {
+            return false;
+        } else {
+            int charCounts[128] = {0};
+            for (char character : strA) {
+                charCounts[character]++;
+            }
+
+            for (char character : strB) {
+                charCounts[character]--;
+            }
+
+            for (int i = 0; i < 128; i++) {
+                if (charCounts[i] != 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}  // namespace q_1_2
+
